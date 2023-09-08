f07aef629db55eac8245b3ba83c40543b4788b1f
aliou <aliou@gmail.com>
2023-09-08 21:23:14 +0200

    design: home hero setting up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import logo from '../assets/logo.svg'
 import { styled } from 'styled-components'
 import { Link } from 'react-router-dom'
 import { FaSearch, FaSearchPlus, FaSearchengin, FaSignOutAlt, FaXbox } from 'react-icons/fa'
-import { FaXmark } from 'react-icons/fa6'
 import { signOut } from 'firebase/auth'
 import { firebaseAuth } from '../utils/firebase-config'
 import jQuery from 'jquery'
@@ -11,23 +10,21 @@ import jQuery from 'jquery'
 const links = [
     { name: "Home", link: "/" },
     { name: "TV Shows", link: "/tv" },
-    { name: "Mouvies", link: "/mouvies" },
+    { name: "Movies", link: "/mouvies" },
     { name: "My List", link: "/mylist" }
 ]
-export default function Navbar({ isSrolled }) {
+export default function Navbar({ isScrolled }) {
     const [showSearch, setShowSearch] = useState(false)
-
     const handleClick = (e) => {
         setShowSearch(true)
         // focus on the input when icon search clicked
         document.getElementById('input-search').focus();
-        console.log(document.getElementById('input-search'));
     }
     return (
         <Container>
             <nav className='w-full'>
-                <div className="content-nav flex justify-around text-white p-[25px]">
-                    <div className="left flex justify-between items-center w-full">
+                <div className={`${isScrolled ? "bg-black" : ""} fixed top-0 w-full content-nav flex text-white p-[25px]`}>
+                    <div className="left flex gap-12 items-center w-full">
                         <div className="img">
                             <a href="/"><img src={logo} className='w-[140px]' alt="logo netflix" /></a>
                         </div>
@@ -46,7 +43,6 @@ export default function Navbar({ isSrolled }) {
                                 <FaSearch className='text-[20px] cursor-pointer' />
                             </span>
                             <input
-
                                 id="input-search"
                                 type="text" placeholder='Search'
                                 className={`${showSearch ? 'block' : 'hidden'} ps-[5px] outline-none rounded-md h-[35px] bg-[#4f4e4ede]`}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,10 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { firebaseAuth } from '../utils/firebase-config';
 import Navbar from '../components/Navbar';
 import { useState } from 'react';
+import hero from '../assets/hero.jpg'
+import { FaPlay } from 'react-icons/fa6'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+
 
 const Home = () => {
     const navigate = useNavigate()
-    const [isScrolled, onScrolled] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
 
     onAuthStateChanged(firebaseAuth, (currentUser) => {
         // if user is not loged in, redirect him to the login page
@@ -15,15 +19,30 @@ const Home = () => {
     })
 
     window.onscroll = () => {
-        console.log(window.pageYOffset);
-        onScrolled(window.pageYOffset === 0 ? false : true)
+        setIsScrolled(window.pageYOffset === 0 ? false : true)
         return () => (window.onscroll = null)
     }
 
 
     return (
         <Container>
-            <Navbar isScrolled={onscroll} />
+            <Navbar isScrolled={isScrolled} />
+            <div className="hero relative">
+                <div className="bg-home w-full">
+                    <img src={hero} alt="Hero image" className='w-screen h-screen ' />
+                </div>
+                <div className="container absolute bottom-[5rem] ms-[5rem]">
+                    <h1 className='text-[5rem] text-white mb-2'>MY NAME</h1>
+                    <div className="buttons flex items-center gap-8">
+                        <button className='flex items-center gap-4 bg-white'>
+                            <FaPlay /> <span>Play</span>
+                        </button>
+                        <button className='flex items-center gap-4 bg-[#7571718c] text-white'>
+                            <AiOutlineInfoCircle /> <span>More info</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </Container>
     );
 }
@@ -32,4 +51,23 @@ export default Home;
 
 const Container = styled.div`
     background-color: black;
+    h1{
+        font-family: 'Josefin Sans', sans-serif;
+        line-height: 1;
+        letter-spacing: -7px;
+    }
+    .hero{
+        transition: all .4s;
+    }
+    .buttons button{
+        border: 1px solid transparent;
+        border-radius: 5px;
+        padding: 3px 25px;
+        font-size: 23px;
+        cursor: pointer;
+        transition: opaicty .3s;
+        &:hover{
+            opacity: 0.8;
+        }
+    }
 `;

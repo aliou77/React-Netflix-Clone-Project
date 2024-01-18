94280d704b7f9acde895d7181c818d383ff1a505
mamda <mamadoualiou@gmail.com>
2024-01-18 11:40:56 +0100

    server setup for api

---
+++ netflix-api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', function(req, res) {
+
+})
+
+
+
+app.listen(6000, console.log("server connected"))
